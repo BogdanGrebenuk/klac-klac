6ca71504acb849366204dcd93085eac0bcfb5162
Danil <dvigundanil@gmail.com>
2021-02-20 15:13:46 +0200

    Added initial table structure

--- app/db/models.py
+++ app/db/models.py
@@ -14,5 +14,40 @@ User = sa.Table(
     sa.Column('email', sa.Text, nullable=False, unique=True),
     sa.Column('password', sa.Text, nullable=False),
     sa.Column('role', sa.Text, nullable=False),
-    sa.Column('token', sa.Text, nullable=True)
+    sa.Column('token', sa.Text, nullable=True),
+    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id"), nullable=True),
+    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id"), nullable=True)
+)
+
+Driver = sa.Table(
+    'driver',
+    metadata,
+    sa.Column('id', sa.Text, primary_key=True),
+    sa.Column('user_id', sa.Text, sa.ForeignKey("user.id"), nullable=False),
+)
+
+Passenger = sa.Table(
+    'passenger',
+    metadata,
+    sa.Column('id', sa.Text, primary_key=True),
+    sa.Column('user_id', sa.Text, sa.ForeignKey("user.id"), nullable=False),
+)
+
+Agreement = sa.Table(
+    'agreement',
+    metadata,
+    sa.Column('id', sa.Text, primary_key=True),
+    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id"), nullable=False),
+    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id"), nullable=False),
+)
+
+Order = sa.Table(
+    'order',
+    metadata,
+    sa.Column('id', sa.Text, primary_key=True),
+    sa.Column('from', sa.Text, nullable=False),
+    sa.Column('to', sa.Text, nullable=False),
+    sa.Column('status', sa.Text, nullable=False),
+    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id"), nullable=True),
+    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id"), nullable=False),
 )

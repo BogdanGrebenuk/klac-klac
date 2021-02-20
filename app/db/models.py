8d3c72de24bac4cf6506420d86663caabb8446e6
BogdanGrebenuk <bogdan020699@gmail.com>
2021-02-20 23:42:36 +0200

    Add cascade delete to the most of relations

--- app/db/models.py
+++ app/db/models.py
@@ -23,7 +23,7 @@ Driver = sa.Table(
     'driver',
     metadata,
     sa.Column('id', sa.Text, primary_key=True),
-    sa.Column('user_id', sa.Text, sa.ForeignKey("user.id"), nullable=False),
+    sa.Column('user_id', sa.Text, sa.ForeignKey("user.id", ondelete="CASCADE"), nullable=False),
     sa.Column('car_image', sa.Text,),
 )
 
@@ -31,7 +31,7 @@ Passenger = sa.Table(
     'passenger',
     metadata,
     sa.Column('id', sa.Text, primary_key=True),
-    sa.Column('user_id', sa.Text, sa.ForeignKey("user.id"), nullable=False),
+    sa.Column('user_id', sa.Text, sa.ForeignKey("user.id", ondelete="CASCADE"), nullable=False),
 )
 
 Order = sa.Table(
@@ -42,21 +42,21 @@ Order = sa.Table(
     sa.Column('to', sa.Text, nullable=False),
     sa.Column('status', sa.Text, nullable=False),
     sa.Column('image', sa.Text, nullable=True),
-    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id"), nullable=True),
-    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id"), nullable=False),
+    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id", ondelete="CASCADE"), nullable=True),
+    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id", ondelete="CASCADE"), nullable=False),
 )
 
 Agreement = sa.Table(
     'agreement',
     metadata,
     sa.Column('id', sa.Text, primary_key=True),
-    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id"), nullable=False),
-    sa.Column('order_id', sa.Text, sa.ForeignKey("order.id"), nullable=False),
+    sa.Column('driver_id', sa.Text, sa.ForeignKey("driver.id", ondelete="CASCADE"), nullable=False),
+    sa.Column('order_id', sa.Text, sa.ForeignKey("order.id", ondelete="CASCADE"), nullable=False),
 )
 
 BlackList = sa.Table(
     'blacklist',
     metadata,
-    sa.Column('ban_driver_id', sa.Text, sa.ForeignKey("driver.id"), nullable=False),
-    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id"), nullable=False),
+    sa.Column('ban_driver_id', sa.Text, sa.ForeignKey("driver.id", ondelete="CASCADE"), nullable=False),
+    sa.Column('passenger_id', sa.Text, sa.ForeignKey("passenger.id", ondelete="CASCADE"), nullable=False),
 )
